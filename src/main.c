5270f3f440790ce712fb34dd55764906a7032041
Oliver Jakobs <oliverjakobs@gmx.net>
2023-09-27 23:39:56 +0200

    nuklear split into separate files

--- src/main.c
+++ src/main.c
@@ -4,20 +4,6 @@
 
 #include "math/mat4.h"
 
-#include "toolbox/tb_file.h"
-
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_GLFW_GL3_MOUSE_GRABBING
-#define NK_IMPLEMENTATION
-#define NK_GLFW_GL3_IMPLEMENTATION
-#define NK_KEYSTATE_BASED_INPUT
-#include "nuklear.h"
 #include "nuklear_glfw_gl3.h"
 
 static void ignisLogCallback(IgnisLogLevel level, const char* desc)
